bbe586db4797366ad70d024731ac2dbf377f55d2
HELART666 <helart.beats266@gmail.com>
2023-02-13 23:59:46 +0400

    lab 4 final version

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,5 +1,5 @@
 import csv
-import operator
+import os
 
 motorcycles_data = [
     ("article_number", "name", "count", "price"),
@@ -10,19 +10,34 @@ motorcycles_data = [
 ]
 
 
-def write_file_sort_str(read_filename, write_filename, param, method):
+def countFilesInDir(path):
+    # os.walk - генерирует имена файлов в дереве каталогов,
+    # она выдает тройной кортеж (dirpath, dirnames, filenames)
+    print(f'Кол-во файлов в директории {path} = {sum(len(files) for _, _, files in os.walk(path))}')
+
+
+def write_file_sort(read_filename, write_filename, param, method):
+    # открываю файл благодаря with open -
+    # после окончания работы с файлом он автоматом закроется
     with open(read_filename) as file:
+        # создаю экземпляр ридера, чтобы читать файл далее
         reader = csv.DictReader(file)
         with open(write_filename, "w", newline="") as file:
+            # т.к. использую DictWriter - нужно передать имена столбцов
             header = ["article_number", "name", "count", "price"]
+            # создаю экземпляр райтера - чтобы записывать в файл далее, передаю файл и имена столбцов
             writer = csv.DictWriter(file, fieldnames=header)
+            # записываю имена столбцов
             writer.writeheader()
             match method:
                 case 1:
+                    # сортирую список по строковому параметру
                     sortedlist = sorted(reader, key=lambda x: str(x[param]), reverse=False)
                 case 2:
+                    # сортирую список по числовому параметру
                     sortedlist = sorted(reader, key=lambda x: int(x[param]), reverse=False)
             for i in sortedlist:
+                # записываю построчно результат в файл new_data.csv
                 writer.writerow(i)
                 print(i)
 
@@ -32,8 +47,9 @@ def request_user():
     method = int(input())
     print("Введите параметр, по которому нужно произвести сортировку: \narticle_number, name, count, price")
     param = input()
-    write_file_sort_str("data.csv", "new_data.csv", param, method)
+    write_file_sort("data.csv", "new_data.csv", param, method)
     print("Готово! Проверьте файл new_data.csv!")
 
 
+print(countFilesInDir("C:/Users/solda/Desktop/PythonLabs/Lab3"))
 request_user()
--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -1,79 +1,106 @@
+import csv
+
+
 class Product:
-    def __init__(self, item_id: int):
-        self.id = item_id
+    def __init__(self, article_number: int):
+        self.article_number = article_number
 
     def __repr__(self):
-        print(f"{self.id}")
-
-
-class Motorcycle(Product):
-    values = []
-    it = 0
-
-    def __init__(self, article_number: int, name: str, count: int, price: int):
-        super().__init__(article_number)
-        self.name = name
-        self.count = count
-        self.price = price
-
-    def generator(self):
-        self.it = 0
-
-        while self.it < len(self.values):
-            yield self.values[self.it]
-            self.it += 1
-
-    def __repr__(self):
-        return f"Motorcycle: ({self.id}, {self.name}, {self.count}, {self.price})"
-
-    def __str__(self):
-        return f"Артикул: {self.id},\n Название: {self.name},\n Количество: {self.count},\n Цена: {self.price}"
-
-    def __setattr__(self, key, value):
-        self.__dict__[key] = value
-
-    def __getitem__(self, item):
-        return self.values[item]
-
-
-class Equipment(Product):
-    values = []
-    it = 0
-
-    def __init__(self, article_number: int, name: str, count: int, price: int):
-        super().__init__(article_number)
-        self.name = name
-        self.count = count
-        self.price = price
-
-    def generator(self):
-        self.it = 0
-
-        while self.it < len(self.values):
-            yield self.values[self.it]
-            self.it += 1
-
-    def __repr__(self):
-        return f"Motorcycle: ({self.id}, {self.name}, {self.count}, {self.price})"
-
-    def __str__(self):
-        return f"Артикул: {self.id},\n Название: {self.name},\n Количество: {self.count},\n Цена: {self.price}"
-
-    def __setattr__(self, key, value):
-        self.__dict__[key] = value
-
-    def __getitem__(self, item):
-        return self.values[item]
+        print(f"{self.article_number}")
 
     @staticmethod
     def total_amount(count, price):
         return count * price
 
 
-helmet = Equipment(1, "Just1 J18 Pulsar", 3, 22990)
-triumph = Motorcycle(2, "Triumph Speed Twin", 1, 1_299_000)
+class Motorcycle(Product):
+    it = 0
 
-print(helmet.__repr__())
-print(helmet.__str__())
-print(triumph.__repr__())
-print(triumph.__str__())
+    def __init__(self, article_number: int, name: str, count: int, price: int, values=None):
+        super().__init__(article_number)
+        if values is None:
+            values = []
+        self.name = name
+        self.count = count
+        self.price = price
+        self.values = [article_number, self.name, self.count, self.price]
+
+    def generator(self):
+        self.it = 0
+
+        while self.it < len(self.values):
+            yield self.values[self.it]
+            self.it += 1
+
+    def __repr__(self):
+        return f"Motorcycle: ({self.article_number}, {self.name}, {self.count}, {self.price})"
+
+    def __str__(self):
+        return f"article_number {self.article_number}, name {self.name}, count {self.count}, price {self.price}"
+
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getitem__(self, item):
+        return self.values[item]
+
+
+# class Equipment(Product):
+#
+#     def __init__(self, article_number: int, name: str, count: int, price: int, values=None):
+#         super().__init__(article_number)
+#         if values is None:
+#             values = []
+#         self.name = name
+#         self.count = count
+#         self.price = price
+#         self.values = [article_number, self.name, self.count, self.price]
+#
+#     def generator(self):
+#         self.it = 0
+#
+#         while self.it < len(self.values):
+#             yield self.values[self.it]
+#             self.it += 1
+#
+#     def __repr__(self):
+#         return f"Motorcycle: ({self.article_number}, {self.name}, {self.count}, {self.price})"
+#
+#     def __str__(self):
+#         return f"article_number {self.article_number}, name {self.name}, count {self.count}, price {self.price}"
+#
+#     def __setattr__(self, key, value):
+#         self.__dict__[key] = value
+#
+#     def __getitem__(self, item):
+#         return self.values[item]
+
+
+def write_file_sort(read_filename, write_filename, param):
+    # открываю файл благодаря with open -
+    # после окончания работы с файлом он автоматом закроется
+    data = []
+    with open(read_filename) as file:
+        # создаю экземпляр ридера, чтобы читать файл далее
+        reader = csv.DictReader(file)
+        with open(write_filename, "w", newline="") as file:
+            # создаю экземпляр райтера - чтобы записывать в файл далее
+            writer = csv.writer(file)
+            for i in reader:
+                data.append(Motorcycle(i["article_number"], i["name"], i["count"], i["price"]))
+            print(data)
+            sortedlist = sorted(data, key=lambda x: x[param], reverse=False)
+            for i in sortedlist:
+                writer.writerow(i)
+
+
+def request_user():
+    print(
+        "Введите параметр, по которому нужно произвести сортировку: \n0 - article_number, 1 - name, 2 - count, 3 - price"
+    )
+    param = int(input())
+    write_file_sort("data.csv", "new_data.csv", param)
+    print("Готово! Проверьте файл new_data.csv!")
+
+
+request_user()
