3eb184d268e6be541ac3fa8ed13132f29900c6f3
HELART666 <helart.beats266@gmail.com>
2023-03-07 18:54:27 +0400

    finally timestamp

--- HomeworkTasks/HomeWork.py
+++ HomeworkTasks/HomeWork.py
@@ -2,9 +2,7 @@ import time
 
 timestamp = time.time()
 
-
-month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
-
+month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 months = ['January', 'February', 'March', 'April', 'May', 'June',
           'July', 'August', 'September', 'October', 'November', 'December']
@@ -27,10 +25,10 @@ while True:
             year += 1
 
 day_of_month = int((timestamp / 86400 - day) % month_days[month]) + 1
-hours = int((timestamp % 86400) // 3600)
+## добавляю 4 чтобы было нормальное Ульяновское время
+hours = int((timestamp % 86400) // 3600) + 4
 minutes = int((timestamp % 3600) // 60)
 seconds = int(timestamp % 60)
 
-date_str = f"{year:04d}-{month+1:02d}-{day_of_month:02d} {hours:02d}:{minutes:02d}:{seconds:02d}"
+date_str = f"{year:04d}-{month + 1:02d}-{day_of_month:02d} {hours:02d}:{minutes:02d}:{seconds:02d}"
 print(date_str)
-
